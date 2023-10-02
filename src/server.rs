071c64b3b0206fc7852cf8890467f63b666496c8
Philpax <me@philpax.me>
2023-10-02 13:56:08 +0200

    Chapter 3

--- src/server.rs
+++ src/server.rs
@@ -1,14 +1,23 @@
 use ambient_api::{
     core::{
-        model::components::model_from_url, physics::components::plane_collider,
-        player::components::is_player, primitives::components::quad, rendering::components::color,
-        transform::components::scale,
+        model::components::model_from_url,
+        physics::components::{cube_collider, dynamic, plane_collider, sphere_collider},
+        player::components::is_player,
+        primitives::{
+            components::{cube, quad},
+            concepts::Sphere,
+        },
+        rendering::components::color,
+        transform::{
+            components::{scale, translation},
+            concepts::Transformable,
+        },
     },
     prelude::*,
 };
 use packages::{
     character_animation::components::basic_character_animations,
-    character_controller::components::{camera_distance, use_character_controller},
+    character_controller::components::use_character_controller, this::components::bouncy_created,
 };
 
 #[main]
@@ -30,9 +39,39 @@ pub fn main() {
                         model_from_url(),
                         packages::base_assets::assets::url("Y Bot.fbx"),
                     )
-                    .with(basic_character_animations(), id)
-                    .with(camera_distance(), -1.0),
+                    .with(basic_character_animations(), id),
             );
+        }
+    });
+
+    for _ in 0..30 {
+        Entity::new()
+            .with(cube(), ())
+            .with(cube_collider(), Vec3::ONE)
+            .with(translation(), (random::<Vec2>() * 20.0 - 10.0).extend(1.))
+            .spawn();
+    }
+
+    fixed_rate_tick(Duration::from_secs_f32(0.5), |_| {
+        Entity::new()
+            .with_merge(Sphere::suggested())
+            .with_merge(Transformable::suggested())
+            .with(scale(), Vec3::ONE * 0.2)
+            .with(
+                translation(),
+                Vec3::X * 10. + (random::<Vec2>() * 2.0 - 1.0).extend(10.),
+            )
+            .with(sphere_collider(), 0.5)
+            .with(dynamic(), true)
+            .with(bouncy_created(), game_time())
+            .spawn();
+    });
+
+    query(bouncy_created()).each_frame(|entities| {
+        for (id, created) in entities {
+            if (game_time() - created).as_secs_f32() > 5.0 {
+                entity::despawn(id);
+            }
         }
     });
 }
